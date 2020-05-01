16a70be787a028bd2779c3261f2b3437936496ea
Andreas Siegel <mail@andreassiegel.de>
2020-05-01 15:46:08 +0200

    refactor: use prompt helpers

--- commands/start.js
+++ commands/start.js
@@ -1,23 +1,23 @@
-const { Input, NumberPrompt } = require('enquirer')
 const { sortBy } = require('lodash')
 
 const { Spinner } = require('../helpers/spinner')
 const { getActivities, startTracking } = require('../helpers/timeular-api-helpers')
 const { feedbackColor } = require('../helpers/colors')
 const parse = require('../helpers/trackingParser')
+const { promptIndex, promptInput } = require('../helpers/prompt')
 const stop = require('./stop')
 
 const start = async argv => {
   const { apiToken, activityName } = argv
   const spinner = new Spinner(feedbackColor('Start tracking...'))
   try {
-    const id = argv.id || await _getActivityIdFromName(apiToken, activityName)
-    const message = argv.message || await _getMessageFromInput()
+    const activityId = argv.activityId || await _getActivityIdFromName(apiToken, activityName)
+    const message = argv.message || await promptInput('Note:')
 
     spinner.start()
     await _stopPrevious(argv)
 
-    const newActivity = await startTracking(apiToken, id, message)
+    const newActivity = await startTracking(apiToken, activityId, message)
     spinner.end()
     console.log('Started tracking: ' + parse(newActivity))
   } catch (err) {
@@ -56,19 +56,9 @@ const _getActivityFromInput = async activities => {
   activities.forEach((activity, index) => {
     console.log(`${index}\t${activity.name}`)
   })
-  const index = await _promptIndex(activities)
+  const index = await promptIndex(activities, 'Number of activity to track:')
   return activities[index].name
 }
-
-const _promptIndex = async activities => new NumberPrompt({
-  name: 'index',
-  message: 'Number of activity to track:',
-  validate: i => i >= 0 && i < activities.length
-}).run()
-
-const _getMessageFromInput = async () => new Input({
-  message: 'Note:'
-}).run()
 
 const _stopPrevious = async argv => {
   try {
